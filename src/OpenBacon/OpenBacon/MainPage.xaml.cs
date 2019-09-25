2a7e2f98b7ec7f73be56225bb2fe5b767b47d67f
Kris Craig <KrisCraig@php.net>
2019-09-25 00:47:25 -0700

    Updated version.

--- src/OpenBacon/OpenBacon/MainPage.xaml.cs
+++ src/OpenBacon/OpenBacon/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +13,13 @@ namespace OpenBacon
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public string GetVersion()
+        {
+            string res = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            return (string.IsNullOrWhiteSpace(res) || !res.Contains(".") ? res : res.Substring(0, res.LastIndexOf(".")) +
+                (res.EndsWith(".1") ? "+develop" : res.EndsWith(".2") ? "+beta" : ""));
         }
     }
 }
