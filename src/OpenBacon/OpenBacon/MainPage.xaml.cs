059f13aabc6f74b67601281012d6936baaae35e5
Kris Craig <KrisCraig@php.net>
2019-09-26 04:35:15 -0700

    Subreddit selection and subscriptions.

--- src/OpenBacon/OpenBacon/MainPage.xaml.cs
+++ src/OpenBacon/OpenBacon/MainPage.xaml.cs
@@ -15,6 +15,22 @@ namespace OpenBacon
         public RedditAPI Reddit { get; private set; }
         public Subreddit Subreddit { get; private set; }
 
+        public IList<Subreddit> Subscriptions
+        {
+            get
+            {
+                return (!SubscriptionsLastUpdated.HasValue || SubscriptionsLastUpdated.Value.AddMinutes(1) <= DateTime.Now
+                    ? GetSubscriptions()
+                    : subscriptions);
+            }
+            private set
+            {
+                subscriptions = value;
+            }
+        }
+        private IList<Subreddit> subscriptions;
+        private DateTime? SubscriptionsLastUpdated;
+
         public MainPage(RedditAPI reddit, string subreddit = "")
         {
             InitializeComponent();
@@ -25,14 +41,49 @@ namespace OpenBacon
         public void LoadSub(string subreddit)
         {
             Subreddit = Reddit.Subreddit(subreddit);
-            ToolbarItem_Subreddits.Text = (!string.IsNullOrWhiteSpace(subreddit) ? Subreddit.Name : "Front Page");
+            ToolbarItem_Subreddits.Text = (!string.IsNullOrWhiteSpace(subreddit) 
+                ? (Subreddit.Name.Length <= 15 ? Subreddit.Name : Subreddit.Name.Substring(0, 12) + "...")
+                : "Front Page");
+
+            ToolbarItem_Spacer.Text = (Subreddit.Name.Length <= 10 ? "     " : "  ");
 
             RefreshToolbar();
+        }
+
+        public IList<Subreddit> GetSubscriptions()
+        {
+            IList<Subreddit> res = new List<Subreddit>();
+            string after = "";
+            List<Subreddit> subs;
+            DateTime now = DateTime.Now;
+            do
+            {
+                subs = Reddit.Account.MySubscribedSubreddits(after: after, limit: 100);
+                foreach (Subreddit subreddit in subs)
+                {
+                    res.Add(subreddit);
+                    after = subreddit.Fullname;
+                }
+            } while (subs.Count > 0 && now.AddMinutes(1) > DateTime.Now);
+
+            Subscriptions = res;
+            SubscriptionsLastUpdated = DateTime.Now;
+
+            return res;
         }
 
         public void RefreshToolbar()
         {
             ToolbarItem_Mail.Icon = (Reddit.Account.Messages.Unread.Count.Equals(0) ? "OpenBacon_Mail" : "OpenBacon_Mail_NewMessages");
+
+            List<string> subs = new List<string>();
+            foreach (Subreddit subreddit in Subscriptions)
+            {
+                subs.Add(subreddit.Name);
+            }
+            subs.Sort();
+
+            ListView_Subreddits.ItemsSource = subs.ToArray();
         }
 
         public string GetVersion()
@@ -44,7 +95,7 @@ namespace OpenBacon
 
         private void ToolbarItemSubreddits_Clicked(object sender, EventArgs e)
         {
-            // TODO
+            Popup_Subreddits.IsVisible = true;
         }
 
         private void ToolbarItemMail_Clicked(object sender, EventArgs e)
@@ -60,6 +111,17 @@ namespace OpenBacon
         private void ToolbarItemSearch_Clicked(object sender, EventArgs e)
         {
             // TODO
+        }
+
+        private void ListView_Subreddits_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            LoadSub(((ListView)sender).SelectedItem.ToString());
+            Popup_Subreddits.IsVisible = false;
+        }
+
+        private void Popup_Subreddits_OutClick(object sender, EventArgs e)
+        {
+            Popup_Subreddits.IsVisible = false;
         }
     }
 }
